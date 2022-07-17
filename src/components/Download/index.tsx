73eb43fef0f47cc916f1e5970384c1402c001823
arifullinrd <arifullinrd@lad24.ru>
2022-07-17 19:31:29 +0300

    Download html and css

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -9,7 +9,30 @@ import ResultForm from 'components/ResultForm';
 const Download = () => {
   const [visiblePreview, setVisiblePreview] = useState(false);
 
-  const onDownloadAsHtml = () => {};
+  const onDownloadAsHtml = () => {
+    const newDoc = document.implementation.createHTMLDocument(
+      'Result Form Document',
+    );
+    const styles = document.getElementsByTagName('style');
+    const resultFormElement = document.getElementById('resultFormPreview');
+    for (let i = 0; i < styles.length; i += 1) {
+      const style = document.createElement('style');
+      style.innerHTML = styles[i].innerHTML;
+      newDoc.head.append(style);
+    }
+    if (resultFormElement) {
+      newDoc.body.append(resultFormElement.cloneNode(true));
+    }
+
+    newDoc.head.append();
+    const blob = new Blob([new XMLSerializer().serializeToString(newDoc)], {
+      type: 'text/html;charset=utf-8',
+    });
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'Result Form';
+    a.click();
+  };
 
   return (
     <>
